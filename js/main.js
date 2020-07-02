ff9ae5c64cc3328049e9fc52eee09b54e65f6396
Mathieu Deriquehem <mathieuderiquehem@gmail.com>
2020-07-02 16:05:13 +0200

    Welcome message

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//Chifoumi game
+
+// Display of a welcome message
+
+alert("Bienvenue sur le jeu du Chifoumi !"); //Message d'accueil
+alert("Amusez-vous bien et profitez de votre pause chers collégues !" + " " + String.fromCodePoint(0x1F609)); //Suite du message d'accueil
+
